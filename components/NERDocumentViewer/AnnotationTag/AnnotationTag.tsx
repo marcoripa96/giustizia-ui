9a0fdd78fefb5a7408d53fb87e5a832d72e1aac1
marcoripa96 <marco.ripa.96@gmail.com>
2022-04-12 16:16:39 +0200

    Fix typings

--- components/NERDocumentViewer/AnnotationTag/AnnotationTag.tsx
+++ components/NERDocumentViewer/AnnotationTag/AnnotationTag.tsx
@@ -14,11 +14,11 @@ type TagProps = {
 const Tag = styled.span<TagProps>`
   padding: 2px 5px;
   border-radius: 6px;
-  background: ${({ type }) => annotationTypes[type].color};
+  background: ${({ type }: any) => annotationTypes[type as keyof typeof annotationTypes].color};
   transition: background 250ms ease-out;
 
   &:hover {
-    background: ${({ type }) => darken(0.15, annotationTypes[type].color)}
+    background: ${({ type }: any) => darken(0.15, annotationTypes[type as keyof typeof annotationTypes].color)}
   }
 `
 
@@ -28,7 +28,7 @@ const AnnotationType = styled.span<TagProps>`
   text-transform: uppercase;
   margin-left: 6px;
   padding: 0 3px;
-  background: ${({ type }) => darken(0.1, annotationTypes[type].color)};
+  background: ${({ type }: any) => darken(0.1, annotationTypes[type as keyof typeof annotationTypes].color)};
   border-radius: 4px;
 `
 
