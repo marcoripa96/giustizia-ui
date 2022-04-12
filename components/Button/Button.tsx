4b104c23ac36953bd4e370296be51cb43aaeba36
marcoripa96 <marco.ripa.96@gmail.com>
2022-04-12 16:14:51 +0200

    Fix typings

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
 
-const Button = styled.button({
-  padding: '10px 20px',
-  outline: 'none',
-  border: '2px solid #000',
-  borderRadius: '6px',
-  fontSize: '14px',
-  fontWeight: 600,
-  backgroundColor: '#000',
-  color: '#FFF',
-  '&:hover': {
-    backgroundColor: '#2e2e2e',
-  },
-  '&:active': {
-    backgroundColor: '#3b3b3b',
-  },
-  cursor: 'pointer'
-})
+const Button = styled.button`
+  padding: 10px 20px;
+  outline: none;
+  border: 2px solid #000;
+  border-radius: 6px;
+  font-size: 14px;
+  font-weight: 600;
+  background-color: #000;
+  color: #FFF;
+  &:hover {
+    background-color: #2e2e2e;
+  };
+  &:active {
+    background-color: #3b3b3b;
+  };
+  cursor: pointer;
+`
 
 export default Button;
