55105927a32012cddc3246a6ba13a8ddefa473d6
marcoripa96 <marco.ripa.96@gmail.com>
2022-04-20 17:58:24 +0200

    Add loading on page change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import { Global } from '@emotion/react'
 import styled from '@emotion/styled'
 import type { AppProps } from 'next/app'
 import GlobalStyles from '../styles/globalStyles'
+import NextNProgress from "nextjs-progressbar";
 
 const Layout = styled.div`
   min-height: 100vh;
-  background: #FFFFFF;;
+  background: #FFFFFF;
 `
 
 function MyApp({ Component, pageProps, router }: AppProps) {
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       <Global styles={GlobalStyles} />
       <Layout>
         {!(['/login'].includes(router.pathname)) && <MainToolbar />}
+        <NextNProgress color='rgb(75 85 99)' />
         <Component {...pageProps} />
       </Layout>
     </>
