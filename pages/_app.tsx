e37af2d3bc2329081fc4f0dc6b3adfb96908668c
marcoripa96 <marco.ripa.96@gmail.com>
2022-05-19 15:03:37 +0200

    Fix deploy dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,54 +1,56 @@
-import { MainToolbar } from '@/components'
-import { Global } from '@emotion/react'
-import styled from '@emotion/styled'
-import type { AppProps } from 'next/app'
-import GlobalStyles from '../styles/globalStyles'
-import NextNProgress from "nextjs-progressbar";
+import { MainToolbar } from '@/components';
+import { Global } from '@emotion/react';
+import styled from '@emotion/styled';
+import type { AppProps } from 'next/app';
+import GlobalStyles from '../styles/globalStyles';
+import NextNProgress from 'nextjs-progressbar';
 import { withTRPC } from '@trpc/next';
-import { AppRouter } from '@/server/routers/_app'
-import { NextUIProvider } from '@nextui-org/react'
-import { NextPage } from 'next'
-import { ReactElement, ReactNode } from 'react'
+import { AppRouter } from '@/server/routers/_app';
+import { NextUIProvider } from '@nextui-org/react';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
+  Component: NextPageWithLayout;
+};
 
 const Layout = styled.div`
   min-height: 100vh;
-  background: #FFFFFF;
-`
+  background: #ffffff;
+`;
 
 const getTRPCUrl = () => {
   if (process.env.NEXT_PUBLIC_PROD_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_PROD_VERCEL_URL}/api/trpc`
+    return `https://${process.env.NEXT_PUBLIC_PROD_VERCEL_URL}/api/trpc`;
+  }
+  if (process.env.NEXT_PUBLIC_DEV_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_DEV_VERCEL_URL}/api/trpc`;
   }
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`;
   }
   return 'http://localhost:3000/api/trpc';
-}
+};
 
 function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <>
       <Global styles={GlobalStyles} />
       <NextUIProvider>
         <Layout>
-          <NextNProgress color='rgb(75 85 99)' showOnShallow={false} />
+          <NextNProgress color="rgb(75 85 99)" showOnShallow={false} />
           {getLayout(<Component {...pageProps} />)}
           {/* {!(['/login'].includes(router.pathname)) && <MainToolbar />} */}
         </Layout>
       </NextUIProvider>
     </>
-  )
+  );
 }
 
 export default withTRPC<AppRouter>({
@@ -72,5 +74,3 @@ export default withTRPC<AppRouter>({
    */
   ssr: false,
 })(MyApp);
-
-
