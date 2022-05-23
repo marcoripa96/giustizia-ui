e112b62775d217aebd07d86e3515486305140b52
marcoripa96 <marco.ripa.96@gmail.com>
2022-05-23 16:43:20 +0200

    Fix types

--- modules/document/SidebarAddAnnotation/Tree/utils.ts
+++ modules/document/SidebarAddAnnotation/Tree/utils.ts
@@ -113,57 +113,6 @@ export const getPathToNode = (
   return getPathToNode(obj, node.parent, path);
 };
 
-export const addSubNode = (
-  children: ChildTreeItem[] | undefined,
-  splittedPath: string[],
-  node: ChildTreeItem
-): ChildTreeItem[] => {
-  if (splittedPath.length === 0) {
-    if (!children) {
-      return [node];
-    }
-    return [...children, node];
-  }
-  const key = splittedPath.shift() as string;
-
-  return children!.map((child) => {
-    if (child.key === key) {
-      return {
-        ...child,
-        children: addSubNode(child.children, splittedPath, node)
-      };
-    }
-    return child;
-  });
-};
-
-export const addNode = (
-  items: TreeItem[],
-  path: string,
-  node: TreeItem | ChildTreeItem
-): TreeItem[] => {
-  if (path === "") {
-    if (!isTopLevelItem(node)) {
-      throw Error("You are trying to add a child node as parent node.");
-    }
-    return [...items, node];
-  }
-
-  const splittedPath = path.split(".");
-
-  const key = splittedPath.shift() as string;
-
-  return items.map((item) => {
-    if (item.key === key) {
-      return {
-        ...item,
-        children: addSubNode(item.children, splittedPath, node)
-      };
-    }
-    return item;
-  });
-};
-
 export const deleteNode = (items: TreeItem[], flatItems: FlatTreeObj, path: string) => {
 
 }
