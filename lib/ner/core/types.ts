bc803af829ef7219c3d4e9a2651af99b7afc418d
marcoripa96 <marco.ripa.96@gmail.com>
2022-07-27 10:10:48 +0200

    Refactor nesting [WIP]

--- lib/ner/core/types.ts
+++ lib/ner/core/types.ts
@@ -21,57 +21,27 @@ export type AnnotationFeatures<T> = T & {
 export type ContentNode<T> = EntityNode<T> | TextNode;
 
 export type TextNode = {
-  type: 'text',
+  type: 'text';
   key: number;
   text: string;
   start: number;
   end: number;
-}
-
-export type NestedEntity = {
-  typesMap: Record<string, number>;
-  types: string[];
-  // first annotation
-  // annotationId: number;
-  // type: string;
-  // moreTypes: Set<> {
-  //   annotationId: number;
-  //   type: string;
-  // }[]
-  // [annotationId: number]: string[]
-  // type: string;
-  // // id of the annotation so that I can go back to it
-  // annotationId: number;
 }
 
 export type EntityNode<T> = {
   // type of the node so that typescript can discriminate between nodes
   type: 'entity';
-  // id of the node
   key: number;
-  // text for this node
-  text: string;
-  // start offset of the node
   start: number;
-  // end offset of the node
   end: number;
-  // annotations contained in this node as a map
-  annotations: Record<number, Annotation<T>>;
-  // annotation ids
-  nesting: number[];
-  // types for the nodes (multi type annotations)
-  // types: EntityNodeType
+  annotation: Annotation<T>;
+  children: ContentNode<T>[];
 }
 
 export type SectionNode<T, U> = {
   key: number;
-  text: string;
   start: number;
   end: number;
   annotation: Annotation<U>;
   contentNodes: ContentNode<T>[];
-}
-
-export type DocumentNER = {
-
 }
