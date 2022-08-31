a570a3f5e5c4a439de4dc2f5b5850ed79cf25e50
marcoripa96 <marco.ripa.96@gmail.com>
2022-08-31 15:17:45 +0200

    Fix redirects in dev mode

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const devRedirects = [
-  {
-    source: '/login',
-    destination: '/infer',
-    permanent: true,
-  }
-];
-
-const getRedirects = () => {
-  const dev = process.env.NODE_ENV === 'development' ? devRedirects : [];
-  return [
-    {
-      source: '/',
-      destination: '/infer',
-      permanent: true,
-    },
-    // ...dev
-  ]
-}
-
 
 const nextConfig = {
   reactStrictMode: true,
@@ -30,7 +10,13 @@ const nextConfig = {
     domains: ['upload.wikimedia.org'],
   },
   async redirects() {
-    return getRedirects();
+    return [
+      {
+        source: '/',
+        destination: '/infer',
+        permanent: true,
+      },
+    ]
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 }
