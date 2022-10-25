60658caebdffdf8f64a2bc1786bac92125dd130a
marcoripa96 <marco.ripa.96@gmail.com>
2022-10-25 12:31:25 +0200

    Add support for no login and redirect to document page

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,20 @@
 import { NextRequest, NextResponse } from "next/server"
 
+
+
 export default function handler(req: NextRequest) {
+  if (process.env.NO_LOGIN === 'true') {
+    if (process.env.MODE === 'statale') {
+      if (!(req.nextUrl.pathname.match(/\/documents\/\d+/)) && !req.nextUrl.pathname.includes('api')) {
+        const url = req.nextUrl.clone();
+
+        url.pathname = `/404`;
+        return NextResponse.rewrite(url);
+      }
+    }
+    return NextResponse.next();
+  }
+
   const token = req.cookies['next-auth.session-token'] || req.cookies['__Secure-next-auth.session-token'];
 
   const authorized = !!token;
@@ -10,7 +24,7 @@ export default function handler(req: NextRequest) {
       const redirectUrl = new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/infer`, req.url);
       return NextResponse.redirect(redirectUrl);
     }
- } else {
+  } else {
     if (req.nextUrl.pathname !== '/sign-in' && !req.nextUrl.pathname.includes('/api/auth')) {
       const redirectUrl = new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/sign-in`, req.url);
       return NextResponse.redirect(redirectUrl);
