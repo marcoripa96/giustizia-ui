71e55db800104946d17980297beb0ec462b448c1
Riccardo Pozzi <pozzir00@gmail.com>
2022-11-03 17:57:02 +0100

    [Workaround] Fix body too big
    When saving the modified annotation set an error is often trown because the body size of the call is over 1mb; increase limit to 10mb.

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,6 +1,14 @@
 import { appRouter } from '@/server/routers/_app';
 import * as trpcNext from '@trpc/server/adapters/next';
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+}
+
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter
